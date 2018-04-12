25aa60cc59a0befe92716713f66da7e274693fed
mohrai <mohrai@DIN51002765.corp.capgemini.com>
2018-04-12 17:36:23 +0530

    Batch processing with CSV

---
+++ batchprocessing-poc3/src/main/java/com/cg/transform/TransformToCollection.java
@@ -0,0 +1,45 @@
+package com.cg.transform;
+
+public class TransformToCollection {
+
+	private String policyID;
+	private String statecode;
+	private String country;
+	private String eq_site_limit;
+	private String hu_site_limit;
+	
+	
+	public String getPolicyID() {
+		return policyID;
+	}
+	public void setPolicyID(String policyID) {
+		this.policyID = policyID;
+	}
+	public String getStatecode() {
+		return statecode;
+	}
+	public void setStatecode(String statecode) {
+		this.statecode = statecode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getEq_site_limit() {
+		return eq_site_limit;
+	}
+	public void setEq_site_limit(String eq_site_limit) {
+		this.eq_site_limit = eq_site_limit;
+	}
+	public String getHu_site_limit() {
+		return hu_site_limit;
+	}
+	public void setHu_site_limit(String hu_site_limit) {
+		this.hu_site_limit = hu_site_limit;
+	}
+	
+	
+
+}
