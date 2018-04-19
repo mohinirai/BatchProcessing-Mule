b340ca8a7a67072f7e49b62332934b74fe742827
mohrai <mohrai@DIN51002765.corp.capgemini.com>
2018-04-19 09:44:12 +0530

    Send the attached CSV from postman and process it in batches and log all the steps of batch processing.

--- batchprocessing-poc3/src/main/java/com/cg/transform/TransformToCollection.java
+++ batchprocessing-poc3/src/main/java/com/cg/transform/TransformToCollection.java
@@ -7,6 +7,19 @@ public class TransformToCollection {
 	private String country;
 	private String eq_site_limit;
 	private String hu_site_limit;
+	private String fl_site_limit;
+	private String fr_site_limit;
+	private String tiv_2011;
+	private String tiv_2012;
+	private String eq_site_deductible;
+	private String hu_site_deductible;
+	private String fl_site_deductible;
+	private String fr_site_deductible;
+	private String point_latitude;
+	private String point_longitude;
+	private String line;
+	private String construction;
+	private String point_granularity;
 	
 	
 	public String getPolicyID() {
@@ -39,7 +52,93 @@ public class TransformToCollection {
 	public void setHu_site_limit(String hu_site_limit) {
 		this.hu_site_limit = hu_site_limit;
 	}
-	
-	
+	public String getFl_site_limit() {
+		return fl_site_limit;
+	}
+	public void setFl_site_limit(String fl_site_limit) {
+		this.fl_site_limit = fl_site_limit;
+	}
+	public String getFr_site_limit() {
+		return fr_site_limit;
+	}
+	public void setFr_site_limit(String fr_site_limit) {
+		this.fr_site_limit = fr_site_limit;
+	}
+	public String getTiv_2011() {
+		return tiv_2011;
+	}
+	public void setTiv_2011(String tiv_2011) {
+		this.tiv_2011 = tiv_2011;
+	}
+	public String getTiv_2012() {
+		return tiv_2012;
+	}
+	public void setTiv_2012(String tiv_2012) {
+		this.tiv_2012 = tiv_2012;
+	}
+	public String getEq_site_deductible() {
+		return eq_site_deductible;
+	}
+	public void setEq_site_deductible(String eq_site_deductible) {
+		this.eq_site_deductible = eq_site_deductible;
+	}
+	public String getHu_site_deductible() {
+		return hu_site_deductible;
+	}
+	public void setHu_site_deductible(String hu_site_deductible) {
+		this.hu_site_deductible = hu_site_deductible;
+	}
+	public String getFl_site_deductible() {
+		return fl_site_deductible;
+	}
+	public void setFl_site_deductible(String fl_site_deductible) {
+		this.fl_site_deductible = fl_site_deductible;
+	}
+	public String getFr_site_deductible() {
+		return fr_site_deductible;
+	}
+	public void setFr_site_deductible(String fr_site_deductible) {
+		this.fr_site_deductible = fr_site_deductible;
+	}
+	public String getPoint_latitude() {
+		return point_latitude;
+	}
+	public void setPoint_latitude(String point_latitude) {
+		this.point_latitude = point_latitude;
+	}
+	public String getPoint_longitude() {
+		return point_longitude;
+	}
+	public void setPoint_longitude(String point_longitude) {
+		this.point_longitude = point_longitude;
+	}
+	public String getLine() {
+		return line;
+	}
+	public void setLine(String line) {
+		this.line = line;
+	}
+	public String getConstruction() {
+		return construction;
+	}
+	public void setConstruction(String construction) {
+		this.construction = construction;
+	}
+	public String getPoint_granularity() {
+		return point_granularity;
+	}
+	public void setPoint_granularity(String point_granularity) {
+		this.point_granularity = point_granularity;
+	}
+	@Override
+	public String toString() {
+		return "TransformToCollection [policyID=" + policyID + ", statecode=" + statecode + ", country=" + country
+				+ ", eq_site_limit=" + eq_site_limit + ", hu_site_limit=" + hu_site_limit + ", fl_site_limit="
+				+ fl_site_limit + ", fr_site_limit=" + fr_site_limit + ", tiv_2011=" + tiv_2011 + ", tiv_2012="
+				+ tiv_2012 + ", eq_site_deductible=" + eq_site_deductible + ", hu_site_deductible=" + hu_site_deductible
+				+ ", fl_site_deductible=" + fl_site_deductible + ", fr_site_deductible=" + fr_site_deductible
+				+ ", point_latitude=" + point_latitude + ", point_longitude=" + point_longitude + ", line=" + line
+				+ ", construction=" + construction + ", point_granularity=" + point_granularity + "]";
+	}
 
 }
